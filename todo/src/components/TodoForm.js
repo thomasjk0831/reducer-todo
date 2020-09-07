d3a6bf7b23fc8898f1bffa57d133fc913cb2e4f7
Thomas Kim <Thomasjk0831@gmail.com>
2020-09-07 13:50:17 -0400

    all reducer functionality working. need to add ability clear input after submit and add some styling

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,109 +1,41 @@
 import React, { useState, useReducer } from 'react'
-import Todo from './Todo'
+import { initialState, reducer } from '../reducers/TodoReducers'
 
-const toDoListData = [
-    {
-      name: "Wake up",
-      id: 111,
-      done: false
-    },
-  
-    {
-      name: "Eat breakfast",
-      id: 112,
-      done: false
-    },
-    {
-      name: "Attend lecture",
-      id: 113,
-      done: false
-    },
-    {
-      name: "Do project",
-      id: 114,
-      done: false
-    },
-    {
-      name: "Exercise",
-      id: 115,
-      done: false
-    },
-    {
-      name: "Wash car",
-      id: 116,
-      done: false
-    },
-  
-  ]
 
 
 function TodoForm(){
     
-    const [list, setList] =  useState(toDoListData)
     const [task, setTask] = useState('')
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     const handleInput = (e)=>{
        setTask(e.target.value)
       }
 
-      const handleSubmit = (e) => {
-          e.preventDefault()
-          setList([...list, 
-            {
-                name : task,
-                id: Date.now(),
-                done: false
-            }
-        ])
-        setTask('')
-      }
-    
-      const toggleDone = (id) => {
-          const temp = list.map((item)=>{
-              if(id === item.id){
-                  return {
-                      ...item,
-                      done : !item.done
-                  }
-              }
-              else
-              return item
-          })
-          setList(temp)
-      }
-
-      const clearCompleted = () => {
-          const temp = list.filter((item)=>{
-              if (item.done === false)
-              return item
-              else
-              return
-          })
-
-          setList(temp)
-      }
+  
 
       return(
           <div className="form-container">
-          <form onSubmit={handleSubmit}>
-              <label>
+          <div>
                   <input 
                    name = "name"
                    type = "text"
                    value = {task}
                    onChange = {handleInput}>
                   </input>
-              </label>
               <div>
-                <button >Add to List</button>
+                <button 
+                onClick={()=>{dispatch({type: 'ADD_TODO', payload: task})}}>Add to List</button>
               </div>
-          </form>
+          </div>
           <div className="clear">
-            <button onClick={clearCompleted}>Clear Completed</button>
+            <button onClick={()=>{dispatch({type: "CLEAR_COMPLETED"})}}>Clear Completed</button>
           </div>
             {
-                list.map((task)=>{
-                    return <Todo task = {task} toggleDone = {()=>{toggleDone(task.id)}} />
+                state.map((task)=>{
+                    return <div onClick={()=>{dispatch({type: "TOGGLE_DONE", id: task.id})}}>
+                    {task.name}
+                </div>
                 })
             }
           </div>
